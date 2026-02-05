45bb1965015e829d10bc39a67e47540c5f36ccfd
Karoliny <karolinyapneppel@gmail.com>
2026-02-05 10:31:16 -0300

    Alterações finais

---
+++ src/app/services/auth-guard.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuardService implements CanActivate {
+
+  constructor(private router: Router) { }
+
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean {
+
+    const authToken = sessionStorage.getItem('auth-token');
+
+    if (authToken) {
+      return true;
+    } else {
+      this.router.navigate(['/login']);
+      return false;
+    }
+  }
+}
--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -8,11 +8,22 @@ import { tap } from 'rxjs';
 })
 export class LoginService {
 
+  apiUrl: String = ""
+
   constructor(private httpClient: HttpClient) {
   }
 
-  login(name: string, password: string){
-    return this.httpClient.post<LoginResponse>("/login", { name, password }).pipe(
+  login(email: string, password: string){
+    return this.httpClient.post<LoginResponse>(this.apiUrl + "/login", { email, password }).pipe(
+      tap((value) =>  {
+        sessionStorage.setItem("auth-token", value.token)
+        sessionStorage.setItem("username", value.name)
+      })
+    )
+  }
+
+  signup(name: string, email: string, password: string){
+    return this.httpClient.post<LoginResponse>(this.apiUrl + "/register", { name, email, password }).pipe(
       tap((value) =>  {
         sessionStorage.setItem("auth-token", value.token)
         sessionStorage.setItem("username", value.name)
